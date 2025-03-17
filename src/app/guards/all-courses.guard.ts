105d2634161fd32c469f9eb87b0847f98d5341d9
tziviSolo <t0504114734@gmail.com>
2025-03-17 18:39:18 +0200

    After denying access to the app before loging in

--- src/app/guards/all-courses.guard.ts
+++ src/app/guards/all-courses.guard.ts
@@ -1,6 +1,6 @@
 import { CanActivateFn } from '@angular/router';
 
 export const allCoursesGuard: CanActivateFn = (route, state) => {
-  const userDetails = JSON.parse(sessionStorage.getItem('user') ?? "");
+  const userDetails = JSON.parse(sessionStorage.getItem('user') ?? "{}");
   return userDetails.role == "student";
 };
